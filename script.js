bf867de7126c3d3d73e2eb7c4ae41ae49566f403
SujayJakka <101142167+SujayJakka@users.noreply.github.com>
2022-04-22 18:05:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-const toggleButton = document.getElementsByClassName("toggle-button")[0]
-const navbarLinks = document.getElementsByClassName("navbar-links")[0]
-
+var toggleButton = document.getElementsByClassName("toggle-button")[0]
+var navbarLinks = document.getElementsByClassName("navbar-links")[0]
+var navbar = document.getElementsByClassName("navbar")[0]
+var navtext = document.getElementsByClassName("navtext")[0]
 toggleButton.addEventListener("click", () => {
         navbarLinks.classList.toggle("active")
+        navbar.classList.toggle("active")
+        navtext.classList.toggle("active")
 })
